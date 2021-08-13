f4db06544f4c173b7830d7abe5da15c310adeeb4
arshdeepsingh <erarshdeep@live.com>
2021-08-13 19:28:31 +0400

    added missing file

---
+++ TowkDemo/Cell/ShimmerTableViewCell/ShimmerTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NormalTableViewCell.swift
+//  TowkDemo
+//
+//  Created by Arshdeep Singh on 11/08/21.
+//
+
+import UIKit
+
+class ShimmerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
